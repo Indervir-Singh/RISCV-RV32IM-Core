bc3abb02f323ebd7cfd0209c0a14b6d5e6f32af3
Indervir Singh <Indervir Singh>
2026-05-05 00:48:43 +0530

    Added MUL_Unit.sv

---
+++ rtl/MUL_Unit.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05.05.2026 00:48:00
+// Design Name: 
+// Module Name: MUL_Unit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module MUL_Unit(
+
+    );
+endmodule
